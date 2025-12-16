5ca4e129d86c971466bf97f33ac2cab2518ecfef
Gautham <ahgamut@gmail.com>
2025-12-16 15:41:08 -0600

    add suit preference bots

--- src/regi_py/strats/__init__.py
+++ src/regi_py/strats/__init__.py
@@ -3,8 +3,16 @@ from regi_py.core import RandomStrategy
 from regi_py.core import DamageStrategy
 from .basic import DummyStrategy
 from .preserve import PreserveStrategy
+from .suitpref import AllPrefs as AllSuits
+
+STRATEGY_LIST = [
+        RandomStrategy,
+        DamageStrategy,
+        DummyStrategy,
+        PreserveStrategy,
+    ] + AllSuits
 
 STRATEGY_MAP = {
     cls.__strat_name__: cls
-    for cls in [RandomStrategy, DamageStrategy, DummyStrategy, PreserveStrategy]
+    for cls in STRATEGY_LIST
 }
