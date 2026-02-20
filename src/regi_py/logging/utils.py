c3e2b880cb5b8893adfe00d70d21ed15f6e4aaf6
Gautham Venkatasubramanian <ahgamut@gmail.com>
2026-02-20 16:09:12 -0600

    return enemy_pile for every entry

--- src/regi_py/logging/utils.py
+++ src/regi_py/logging/utils.py
@@ -22,6 +22,7 @@ def dump_game(game):
     result["enemy_pile_size"] = len(game.enemy_pile)
     if len(game.enemy_pile) > 0:
         result["current_enemy"] = dump_enemy(game.enemy_pile[0])
+    result["enemy_pile"] = [str(x) for x in game.enemy_pile]
     return result
 
 
@@ -30,7 +31,6 @@ def dump_debug(game):
     result["players"] = [dump_player(player) for player in game.players]
     result["draw_pile"] = [str(x) for x in game.draw_pile]
     result["discard_pile"] = [str(x) for x in game.discard_pile]
-    result["enemy_pile"] = [str(x) for x in game.enemy_pile]
     return result
 
 
