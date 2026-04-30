9d7144e40d7e74cb71483d300c7f19f75a0d1ec5
Gautham Venkatasubramanian <ahgamut@gmail.com>
2026-04-29 23:27:02 -0500

    delete old mcts stuff
    rewrite incoming

--- src/regi_py/rl/__init__.py
+++ src/regi_py/rl/__init__.py
@@ -1,9 +1,4 @@
 
 from .utils import MemoryLog
-from .rl1 import RL1Strategy, RL1Model
-from .mcts import MCTS, MCTSTesterStrategy
-from .batched_mcts import BatchedMCTS
-from .mc1 import MC1Model
-from .mc2 import MC2Model
 
-STRATEGY_LIST = [RL1Strategy]
+STRATEGY_LIST = []
